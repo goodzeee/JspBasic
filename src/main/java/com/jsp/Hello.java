5a397057bba08acfcb75c4abd0fb05cd493d9de2
jihye <qkdtmdqo14@naver.com>
2024-05-08 16:58:13 +0900

    # 톰캣 사용하여 웹 페이지 컨트롤

--- src/main/java/com/jsp/Hello.java
+++ src/main/java/com/jsp/Hello.java
@@ -1,2 +1,35 @@
-package com.jsp;public class Hello {
+package com.jsp;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("hello ! my website !");
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter w = resp.getWriter();
+
+        w.write("<!DOCTYPE html>\n");
+        w.write("<html>\n");
+        w.write("<head>\n");
+        w.write("</head>\n");
+        w.write("<body>\n");
+        w.write("   \t<h1>안녕하십니까??\n");
+        w.write("   </h1>\n");
+        w.write("</body>\n");
+        w.write("</html>");
+    }
+
+
 }
