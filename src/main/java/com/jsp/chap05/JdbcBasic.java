814ce7e50af07bbbb047bebab59429cb3b51112b
jihye <qkdtmdqo14@naver.com>
2024-05-10 17:04:13 +0900

    # mariadb 연결 후 select 작업 & 댄서 예제 DB 작업

--- src/main/java/com/jsp/chap05/JdbcBasic.java
+++ src/main/java/com/jsp/chap05/JdbcBasic.java
@@ -3,6 +3,9 @@ package com.jsp.chap05;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 // 데이터 저장하는 SQL = MariaDB 연결 및 CRUD !
 public class JdbcBasic {
@@ -71,6 +74,81 @@ public class JdbcBasic {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    // UPDATE 기능
+    public void update(int id, String newName, int newAge) {
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            // 1. 연결 드라이버 로딩
+            Class.forName(driverClassName);
+
+            // 2. 데이터베이스 접속
+
+            // 3. 실행할 SQL 생성
+            String sql = "UPDATE tbl_person " +
+                    "SET person_name = ?, person_age = ? " +
+                    "WHERE id = ?";
+
+            // 4. SQL 실행 객체 생성
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            // 5. ? 값 채우기
+            pstmt.setString(1, newName);  // 삭제할 조건절에 id
+            pstmt.setInt(2, newAge);  // 삭제할 조건절에 id
+            pstmt.setInt(3, id);  // 삭제할 조건절에 id
+
+            // 6. 실행 명령
+            // INSERT, UPDATE, DELETE 같은 명령을 사용
+            pstmt.executeUpdate();
+
+            // 7. 데이터베이스 연결 해제
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // SELECT
+    public List<Person> findAll() {
+
+        try (Connection conn = DriverManager.getConnection(url, username, password)){
+
+            Class.forName(driverClassName);
+
+            String sql = "SELECT * FROM tbl_person " +
+                    "ORDER BY id DESC";
+
+            // SQL 실행 객체 생성
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            // ? 채우기
+
+            // 실행 명령 - SELECT 는 다른 executeQuery() 메서드 사용
+            // SELECT 반환 <-> 나머지는 영향
+            // ResultSet : SELECT 의 결과 집합 표를 가져옴
+            ResultSet rs = pstmt.executeQuery();// 자료구조로 반환
+
+            // ResultSet 데이터 가져오기
+            List<Person> people = new ArrayList<>();
+            while (rs.next()) {   // next() -> boolean 리턴 따라서 true 인 동안 반복 실행
+                // 커서가 가리키는 행의 데이터를 하나씩 추출
+                int id = rs.getInt("id");
+                String personName = rs.getString("person_name");
+                int personAge = rs.getInt("person_age");
+
+                Person person = new Person(id, personName, personAge);
+                people.add(person);  // 사람 정보 리스트로 가져와서 추가하기
+
+            }
+            return people;
+//            rs.next();  // 표의 행을 지목하는 커서 (한 번만 사용했으니 첫 행)
+//            rs.next();
+//            rs.next();  // 3번째 행 조회
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
--- src/main/java/com/jsp/chap05/Test.java
+++ src/main/java/com/jsp/chap05/Test.java
@@ -1,5 +1,7 @@
 package com.jsp.chap05;
 
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -8,7 +10,12 @@ public class Test {
 //        jdbc.insert(new Person(300, "조둥짝", 29));
 //        jdbc.insert(new Person(400, "최태풍", 34));
 
-        jdbc.delete(400);
+//        jdbc.delete(400);
+
+//        jdbc.update(300,"민둥산", 19);
+
+        List<Person> people = jdbc.findAll();
+        System.out.println("people = " + people);
     }
 
 }
