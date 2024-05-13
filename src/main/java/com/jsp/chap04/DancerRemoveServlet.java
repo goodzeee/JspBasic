0901eb633563c1ba249a5e3941441e3e822a06e6
jihye <qkdtmdqo14@naver.com>
2024-05-13 12:47:06 +0900

    # 댄서 DB - DELETE 삭제 기능 추가

--- src/main/java/com/jsp/chap04/DancerRemoveServlet.java
+++ src/main/java/com/jsp/chap04/DancerRemoveServlet.java
@@ -1,14 +1,17 @@
 package com.jsp.chap04;
 
 
+import com.jsp.entity.Dancer;
 import com.jsp.repository.DancerJdbcRepo;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/chap04/remove")
 public class DancerRemoveServlet extends HttpServlet {
@@ -27,7 +30,21 @@ public class DancerRemoveServlet extends HttpServlet {
         String id = req.getParameter("id");
         System.out.println("삭제대상 id : " + id);
 
-        // DB에 삭제 명령
+        // 1. DB에 삭제 명령
         repo.delete(id); // id 줄테니 지워줘
+
+        // 2. /chap04/show-list 요청을 자동으로 보냄 (리다이렉션) sendRedirect
+        // (삭제를 하고 댄서 리스트를 보여줘 재사용하는 것 !)
+        resp.sendRedirect("/chap04/show-list"); // 삭제하고 돌아와서 댄서 리스트 다루는 url 재사용 !
+
+//        // retrieve = select 조회하는 기능 !
+//        // 2. 삭제한 후 댄서 리스트 조회
+//        List<Dancer> dancerList = repo.retrieve();
+//        req.setAttribute("dancers", dancerList);
+//
+//        // 3. 적절한 화면 이동 -> 삭제를 하고 삭제된 걸 다시 조회해서 화면에 보여줘 ! 삭제된 댄서 수송 필요 !
+//        RequestDispatcher dp = req.getRequestDispatcher("/WEB-INF/chap04/dancer-list.jsp");
+//        dp.forward(req, resp);
+
     }
 }
